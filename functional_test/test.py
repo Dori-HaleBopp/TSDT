59ed4eb3ce498e106f2bc5ae47d07e7e29049423
PENG ZIJING <dorihalebopp@bupt.edu.cn>
2024-04-18 14:27:06 +0800

    message 14: add a new user

--- functional_test/test.py
+++ functional_test/test.py
@@ -64,13 +64,49 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1:Buy flowers')
         self.wait_for_row_in_list_table('2:Give a gift to Lisi')
 
-        table=self.browser.find_element(By.ID,'id_list_table')
-        rows=table.find_elements(By.TAG_NAME,'tr')
-        self.assertIn('1:Buy flowers',[row.text for row in rows])
-        self.assertIn('2:Give a gift to Lisi',[row.text for row in rows])
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        #张三新建一个待办事项清单
+        self.browser.get(self.live_server_url)
+        inputbox=self.browser.find_element(By.ID,'id_new_item')
+        inputbox.send_keys('Buy flowers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1:Buy flowers')
 
+        #他注意到清单有一个唯一的URL
+        zhangsan_list_url=self.browser.current_url
+        self.assertRegex(zhangsan_list_url,'/lists/.+')
+
+        #现在一个叫王五的用户访问了网站
+        ##我们使用一个新的浏览器会话
+        ##确保张三的信息不会从cookie中泄露出来
+        self.browser.quit()
+        self.browser=webdriver.Chrome()
+
+        #王五访问首页
+        #首页中看不到张三的清单
+        self.browser.get(self.live_server_url)
+        page_text=self.browser.find_element(By.TAG_NAME,'body').text
+        self.assertNotIn('Buy flowers',page_text)
+        self.assertNotIn('BGive a gift to Lisi',page_text)
+
+        #王五输入一个新的待办事项，新建一个清单
+        inputbox=self.browser.find_element(By.ID,'id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1:Buy milk')
+
+        #王五获得了他的唯一URL
+        wangwu_list_url=self.browser.current_url
+        self.assertRegex(wangwu_list_url,'/lists/.+')
+        self.assertNotEqual(wangwu_list_url,zhangsan_list_url)
+
+        #这个页面还是没有张三的清单
+        page_text=self.browser.find_element(By.TAG_NAME,'body').text
+        self.assertNotIn('Buy flowers',page_text)
+        self.assertIn('Buy milk',page_text)
+
+        #两个人都很满意，然后离开了
         
-        #她输入了“gift to girlfriend”
         self.fail('Finish the test!')
 
         #页面再次更新，她的清单中显示了这两个待办事项
