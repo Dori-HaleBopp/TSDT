5c4fbbd963d8538994bbd1f1eae21188e2ad469c
PENG ZIJING <dorihalebopp@bupt.edu.cn>
2024-05-16 15:57:38 +0800

    message 22: use Bootstrap to improve layout

--- notes/settings.py
+++ notes/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+#Static files (CSS, JavaScript, Images)
+#https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL='/static/'
